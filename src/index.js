26f3996bb35679691d9387bb5e95527e2d1c9595
vishalnexios-ops <vishal.nexios@gmail.com>
2025-12-05 15:59:59 +0530

    Update io.js

--- src/index.js
+++ src/index.js
@@ -25,12 +25,12 @@ app.use(cors({
 app.use(express.static('public'));
 app.use(cookieParser());
 const server = http.createServer(app);
-// const io = new Server(server, {
-//     cors: {
-//         origin: "*",
-//         methods: ["GET", "POST"]
-//     }
-// });
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
 
 // Routes
 const userRoutes = require('./Routes/userRoutes');
@@ -45,29 +45,29 @@ app.use('/health', (req, res) => {
 });
 
 
-// Run every minute
-// cron.schedule("* * * * *", async () => {
-//     await sendScheduleMessage();
-// });
+Run every minute
+cron.schedule("* * * * *", async () => {
+    await sendScheduleMessage();
+});
 
-// 🔹 Socket.io connection
-// io.on("connection", (socket) => {
-//     console.log("⚡ Frontend connected via Socket.io:", socket.id);
+🔹 Socket.io connection
+io.on("connection", (socket) => {
+    console.log("⚡ Frontend connected via Socket.io:", socket.id);
 
-//     socket.on("disconnect", () => {
-//         console.log("Frontend disconnected:", socket.id);
-//     });
-// });
+    socket.on("disconnect", () => {
+        console.log("Frontend disconnected:", socket.id);
+    });
+});
 
-// initSocket(io);
+initSocket(io);
 
 mongoose.connect(process.env.MONGO_URI || "mongodb+srv://paras:CZMazeyPcLZTBjd2@atlascluster.qnoiucs.mongodb.net/test?retryWrites=true&w=majority")
     .then(() => {
         console.log('MongoDB connected')
-        // Start server
-        // server.listen(port, () => {
-        //     console.log(`Server running on http://localhost:${port}`);
-        // });
+        Start server
+        server.listen(port, () => {
+            console.log(`Server running on http://localhost:${port}`);
+        });
     })
     .catch(err => console.log(err));
 
